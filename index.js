77899e8170ee7426a72da653e75de0be43c6fc9b
vdiez <diez@outlook.com>
2017-11-24 16:31:51 +0100

    winston is global

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 let winston = require('winston');
 let moment = require('moment');
 
-let logger = new (winston.Logger)({
-    transports: [
-        new (winston.transports.Console)({
-            timestamp: () => moment().format('YYYY-MM-DD HH:mm:ss.SSS'),
-            level: 'silly',
-            colorize: true
-        })
-    ]
-});
+winston.level = 'silly';
+winston.remove(winston.transports.Console);
+winston.add(winston.transports.Console, {colorize: true, timestamp: () => moment().format('YYYY-MM-DD HH:mm:ss.SSS')});
 
-module.exports = logger;
+module.exports = winston;
