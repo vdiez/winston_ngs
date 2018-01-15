16b2035052bf8c801d1fb315695deee90f19fe46
vdiez <diez@outlook.com>
2018-01-15 12:26:32 +0100

    reduced verbosity

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let winston = require('winston');
 let moment = require('moment');
 
-winston.level = 'silly';
+winston.level = 'debug';
 winston.remove(winston.transports.Console);
 winston.add(winston.transports.Console, {colorize: true, timestamp: () => moment().format('YYYY-MM-DD HH:mm:ss.SSS')});
 
